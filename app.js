1eb017dccc4f489a62701a61548fbf31e6e6cd9e
nabilabel123 <nabilabelaroussia2004@gmail.com>
2024-12-27 15:49:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const userRoutes = require('./routes/userRoutes');
 const bodyParser = require('body-parser');
 
 
-// Middleware pour analyser les corps de requêtes JSON
+
 app.use(express.json());
 
-// Routes
+
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
 app.use(bodyParser.json()); // Pour traiter le corps de la requête en JSON
 
-// Exemple de route GET
+
 app.get('/messages', (req, res) => {
   const { sender_id, recipient_id, date_from, date_to, status, limit, offset } = req.query;
 
-  console.log('Received request:', req.query); // Affiche la requête dans la console du serveur
+  console.log('Received request:', req.query); 
   
-  // Crée une réponse contenant les données reçues
+  
   const response = `
     <h1>Received Message Details</h1>
     <ul>
@@ -34,14 +34,8 @@ app.get('/messages', (req, res) => {
     </ul>
   `;
 
-  res.send(response); // Affiche les informations dans le navigateur
+  res.send(response); 
 });
-
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
